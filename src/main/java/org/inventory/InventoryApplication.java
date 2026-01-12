824cdd8aa0fcd5b8bc59a99c30a7f1c641a0453c
Rajashree.k <156102749+shree2105@users.noreply.github.com>
2026-01-12 11:19:28 +0530

    Remove database initialization from main application
    Removed database initialization methods from InventoryApplication.

--- src/main/java/org/inventory/InventoryApplication.java
+++ src/main/java/org/inventory/InventoryApplication.java
@@ -1,11 +1,8 @@
 package org.inventory;
 
-import org.inventory.config.dbconfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import jakarta.annotation.PostConstruct;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
 
 @EnableScheduling
 @SpringBootApplication
@@ -13,11 +10,5 @@ public class InventoryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(InventoryApplication.class, args);
-    }
-
-    @PostConstruct
-    public void init() {
-        dbconfig.createDatabase();
-        dbconfig.initializeTables();
     }
 }
